91a19d98c508c5d0de7fe676bce16f28081df302
gautamdevgrovers <155137805+gautamdevgrovers@users.noreply.github.com>
2023-12-29 12:45:22 +0530

    Update s1.sh
    added wget

--- s1.sh
+++ s1.sh
@@ -1,4 +1,5 @@
 cd ~/Downloads
+sudo apt install wget -y
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 wget https://download.anydesk.com/linux/anydesk_6.3.0-1_amd64.deb
 sudo dpkg -i *.deb
